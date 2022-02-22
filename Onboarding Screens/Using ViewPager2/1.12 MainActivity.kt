31f371a6e83645d3dd261f1e0c684f320701fcdb
Shoikot <96073081+shykat1@users.noreply.github.com>
2022-02-22 15:44:05 +0600

    Create 1.12 MainActivity.kt

---
+++ Onboarding Screens/Using ViewPager2/1.12 MainActivity.kt
@@ -0,0 +1,11 @@
+class MainActivity : AppCompatActivity() {
+    private  lateinit var  binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+    }
+
+}
