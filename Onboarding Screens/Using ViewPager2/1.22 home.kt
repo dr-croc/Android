27ec15006b84a8c43f17f77f5ce0a8ca92d26fe3
Shoikot <96073081+shykat1@users.noreply.github.com>
2022-02-22 15:51:23 +0600

    Create 1.22 home.kt

---
+++ Onboarding Screens/Using ViewPager2/1.22 home.kt
@@ -0,0 +1,9 @@
+
+public class Home_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
