4b1b40fca794a052fa0d8ccb3b4f931b4f6c30d9
Shoikot <96073081+shykat1@users.noreply.github.com>
2022-04-12 14:51:15 +0600

    Create check Internet.kt

---
+++ 0.0 Kotlin/Faq/check Internet.kt
@@ -0,0 +1,12 @@
+fun isConnected(): Boolean{
+        var connectivityManager : ConnectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        var network : NetworkInfo? =  connectivityManager.activeNetworkInfo
+
+        if(network!= null){
+            if(network.isConnected)
+            {
+                return true
+            }
+        }
+        return false
+    }
