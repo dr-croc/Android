e738e14cf04488328807ab8116f845aa055c875a
Shoikot <96073081+shykat1@users.noreply.github.com>
2022-07-19 17:34:59 +0600

    Create binding.kt

---
+++ 0.0 Kotlin/Random/binding.kt
@@ -0,0 +1,8 @@
+// in this binding section you dont have to even type binding 
+        binding.apply {
+            imgSlider.setOnClickListener{}
+            
+            btnAddImageSlider.setOnClickListener{
+                
+            }
+        }
