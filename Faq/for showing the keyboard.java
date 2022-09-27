7f85b079cd890b13b537fd1a5ca84e9e74935c3d
Documentation <96073081+goodies00@users.noreply.github.com>
2022-09-28 02:57:20 +0600

    Create for showing the keyboard.java

---
+++ Faq/for showing the keyboard.java
@@ -0,0 +1,4 @@
+ private void showKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+    }
