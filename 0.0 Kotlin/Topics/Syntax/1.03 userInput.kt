a17008bae36105ae5d2af488866152b8a65187dd
Documentation <96073081+dr-croc@users.noreply.github.com>
2023-02-04 19:17:40 +0600

    Create 1.03 userInput.kt

---
+++ 0.0 Kotlin/Topics/Syntax/1.03 userInput.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+    var scanner = Scanner(System.`in`)
+
+    var int = scanner.nextInt()
+    println(int)
+    println(int:: class)
+
+    var dou = scanner.nextDouble()
+    println(dou)
+    println(dou:: class)
+
+    var st = scanner.next()
+    println(st)
+    println(st::class)
+
+    var boolean = scanner.nextBoolean()
+    println(boolean::class)
+
+
+}
