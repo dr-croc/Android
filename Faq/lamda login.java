11b6b4db2e7c574133a242b6878d685fe07c8b2e
Documentation <96073081+dr-croc@users.noreply.github.com>
2023-03-10 22:13:40 +0600

    Update lamda login.java

--- Faq/lamda login.java
+++ Faq/lamda login.java
@@ -1,3 +1,12 @@
 login_b.btnLogin.setOnClickListener((View v) ->{
             Toast.makeText(getApplicationContext(), "Hola", Toast.LENGTH_SHORT).show();
         });
+
+
+
+bind_reg.txtRegTOlogin.setOnClickListener(v -> {
+        });
+
+bind_reg.btnRegister.setOnClickListener(v -> {
+
+ });
